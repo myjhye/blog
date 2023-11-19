80e591a68c9bf536d5071e9bc36e923e7b073564
myjhye <t01087633799@gmail.com>
2023-11-19 21:46:21 +0900

    페이지 레이아웃 -> 스타일링

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 export default function Footer() {
 
     return (
-        <p>{"This is my blog and my drama | All Right Reserved."}</p>
+        <p className="bg-slate-900 text-white py-2 text-sm text-center">
+            {"This is my blog and my drama | All Right Reserved."}
+        </p>
     )
 }
