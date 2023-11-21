6e881d4cf8cafb776eef28d3a7315b245ffcdd2c
myjhye <t01087633799@gmail.com>
2023-11-21 20:48:37 +0900

    주석

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -1,8 +1,11 @@
 import { Post } from "@/service/posts";
 
+// prop인 posts -> 데이터 타입 명시 -> Post[] 사용해서
 type Props = { posts: Post[] };
 
 export default function PostsGrid({posts}: Props) {
+
+    // posts를 prop으로 받아 -> 화면에 표시
 
     return (
         <ul>
