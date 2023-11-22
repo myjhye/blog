ea16addeba259546b9eabe0e721f9f741477fd7c
myjhye <t01087633799@gmail.com>
2023-11-22 23:05:01 +0900

    필터가능한 포스트 페이지

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,6 +1,20 @@
-export default function PostsPage() {
+import FilterablePosts from "@/components/FilterablePosts";
+import PostsGrid from "@/components/PostsGrid";
+import { getAllPosts, getFeaturedPosts } from "@/service/posts"
+
+export default async function PostsPage() {
+
+    // 모든 블로그 포스트 가져오기
+    const posts = await getAllPosts();
+    // 블로그 포스트에서 -> 중복되지 않는 -> 카테고리 목록 가져오기
+    const categories = [ ...new Set(posts.map((post) => post.category))];
 
     return (
-        <p>post</p>
+        <section>
+            <FilterablePosts 
+                posts={posts}
+                categories={categories}
+            />
+        </section>
     )
 }
---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Post } from "@/service/posts"
+import { useState } from "react";
+import PostsGrid from "./PostsGrid";
+import Categories from "./Categories";
+
+// Props 타입 정의
+type Props = {
+    // 블로그 포스트 배열
+    posts: Post[];
+    // 카테고리 목록 배열
+    categories: string[];
+};
+
+
+// 모든 게시물을 표시하는 -> 'All Posts' 카테고리 정의
+const ALL_POSTS = 'All Posts';
+
+
+export default function FilterablePosts({posts, categories}: Props) {
+
+    // 선택된 카테고리
+    const [selected, setSelected] = useState(ALL_POSTS);
+    
+    // 선택된 카테고리에 따라 -> 게시물 필터링
+    const filtered = selected === ALL_POSTS
+                        ? posts 
+                        : posts.filter((post) => post.category === selected);
+
+    return (
+        <section>
+            <PostsGrid posts={filtered} />
+            <Categories 
+                categories={[ALL_POSTS, ...categories]}
+                selected={selected}
+                onClick={(selected) => setSelected(selected)} 
+            />
+        </section>
+    )
+}
