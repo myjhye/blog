429d213dabe05080fd0668441ca4d79df3863fdf
myjhye <t01087633799@gmail.com>
2023-11-25 16:58:54 +0900

    마크다운 뷰어 -> 상세정보 (스타일링)

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostData } from "@/service/posts";
+import Image from "next/image";
+import {AiTwotoneCalendar} from 'react-icons/ai';
 
 type Props = {
     params: {
@@ -14,9 +16,26 @@ export default async function PostPage({params: {slug}}: Props) {
     const post = await getPostData(slug);
 
     return (
-        <>
-            <h1>{post.title}</h1>
-            <MarkdownViewer content={post.content} />
-        </>
+        <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+            <Image 
+                className="w-full h-1/5"
+                src={`/images/posts/${post.path}.png`} 
+                alt={post.title} 
+                width={760} 
+                height={420} 
+            />
+            <section className="flex flex-col p-4">
+                <div className="flex items-center self-end">
+                    <AiTwotoneCalendar />
+                    <p className="font-semibold ml-2">{post.date.toString()}</p>
+                </div>
+                <h1 className="text-4xl font-bold">{post.title}</h1>
+                <p className="text-xl font-bold">{post.description}</p>
+                
+                {/* 구분선 */}
+                <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
+                <MarkdownViewer content={post.content} />
+            </section>
+        </article>
     )
 }
