5900c27c6519330605eb08328533c3037de36b89
myjhye <t01087633799@gmail.com>
2023-11-25 16:31:32 +0900

    마크다운 뷰어 -> 라이브러리

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostData } from "@/service/posts";
 
 type Props = {
@@ -15,7 +16,7 @@ export default async function PostPage({params: {slug}}: Props) {
     return (
         <>
             <h1>{post.title}</h1>
-            <pre>{post.content}</pre>
+            <MarkdownViewer content={post.content} />
         </>
     )
 }
