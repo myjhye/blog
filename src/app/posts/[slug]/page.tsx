40547447498033a02afeaec6c2b20ae53cb57552
myjhye <t01087633799@gmail.com>
2023-11-26 16:43:36 +0900

    이전, 다음 포스트 카드 -> 컴포넌트

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AdjacentPostCard from "@/components/AdjacentPostCard";
 import PostContent from "@/components/PostContent";
 import { getPostData } from "@/service/posts";
 import Image from "next/image";
@@ -24,9 +25,9 @@ export default async function PostPage({params: {slug}}: Props) {
                 height={420} 
             />
             <PostContent post={post} />
-            <section>
-                {post.prev && <p>{post.prev.title}</p>}
-                {post.next && <p>{post.next.title}</p>}
+            <section className="flex shadow-md">
+                {post.prev && <AdjacentPostCard post={post.prev} type='prev' />}
+                {post.next && <AdjacentPostCard post={post.next} type='next' />}
             </section>
         </article>
     )
