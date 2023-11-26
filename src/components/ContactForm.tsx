10831f71403a49fb92d6b4360d312a8e23d94ab5
myjhye <t01087633799@gmail.com>
2023-11-26 21:58:04 +0900

    주석 -> contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,36 +3,52 @@
 import { ChangeEvent, FormEvent, useState } from "react"
 import Banner, { BannerData } from "./Banner";
 
+// Form 타입 정의
 type Form = {
     from : string;
     subject : string;
     message : string;
 }
+
 export default function ContactForm() {
 
+    // 폼 데이터 -> 유저가 입력한 데이터 저장
     const [form, setForm] = useState<Form>({
         from: '',
         subject: '',
         message: ''
     });
 
+    // 배너 데이터 -> 배너 메세지, 상태 저장
     const [banner, setBanner] = useState<BannerData | null>(null);
 
+    // 입력 필드 값 변경
     const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
 
         const { name, value } = e.target;
+
+        // 상태 업데이트 -> 현재 폼 상태를 -> 이전 상태를 기반으로 업데이트
         setForm(prev => ({
+            // 이전 상태 복사
             ...prev,
+            // 변경된 name 값을 -> 새 value로 설정
             [name]: value
         }));
     }
 
+    // 폼 제출
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
 
         e.preventDefault();
         console.log(form);
         
-        setBanner({ message: '성공', state: 'success' });
+        // 성공 배너 메세지 표시
+        setBanner({ 
+            message: '성공', 
+            state: 'success' 
+        });
+
+        // 3초 후 배너 숨김
         setTimeout(() => {
             setBanner(null);
         }, 3000);
@@ -40,6 +56,7 @@ export default function ContactForm() {
 
     return (
         <section className="w-full max-w-md">
+            {/* 배너가 존재하는 경우에만 렌더링 */}
             { banner && <Banner banner={banner} /> }
             <form onSubmit={onSubmit} className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white">
                 <label className="font-semibold" htmlFor="from">Your Email</label>
