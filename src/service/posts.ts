27633a6f62c0cab0047623f2bc786798493039e6
myjhye <t01087633799@gmail.com>
2023-11-27 23:53:41 +0900

    성능 개선

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 import { readFile } from "fs/promises";
 import path from "path";
+import { cache } from "react";
 
 // Post -> 블로그 포스트 타입 정의
 export type Post = {
@@ -35,23 +36,19 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
 }
 
 
-
 // 모든 블로그 포스트 가져오는 -> 비동기 함수
-// Promise<Post[]> -> 비동기 작업이 완료 되면 -> Post 객체의 배열 반환 
-export async function getAllPosts(): Promise<Post[]> {
-
-    // process.cwd() -> 현재 작업 디렉토리
+export const getAllPosts = cache(async () => {
+    
     // data 디렉토리 안에 있는 posts.json 파일 지정
     const filePath = path.join(process.cwd(), 'data', 'posts.json');
 
-    // readFile -> 파일 읽어오기
-    // utf-8 -> 파일 내용을 문자열로 읽어오기
     return readFile(filePath, 'utf-8')
         // 읽어온 json 문자열을 -> javascript 객체로 파싱 -> 화면에서 읽을 수 있게
         .then<Post[]>(JSON.parse)
         // 게시물을 날짜 순으로 정렬 -> 내림차순 -> 최신날짜부터
         .then(posts => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
-}
+})
+
 
 
 
